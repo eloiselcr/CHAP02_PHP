a145e107cb6962b5ca77f0f8287b1865ea1927fd
Vexliar <eloise.lecronier@gmail.com>
2022-11-16 17:29:49 +0100

    changement erreur

--- Exercices/Exercice_2/exo2.1_tableauindinum.php
+++ Exercices/Exercice_2/exo2.1_tableauindinum.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exo 2.1 - Les tableaux PHP a indice numerique</title>
         <!-- 
-        ----------- EXO 1 - LES TABLEAUX PHP A INDICE NUMERIQUE -----------
+        ----------- EXO 2.1 - LES TABLEAUX PHP A INDICE NUMERIQUE -----------
         
        Créer un tableau php qui contient 5 chiffres.
        Afficher les 5 chiffres à la suite grace au parcours du tableau dans une boucle for.
     --> 
 </head>
 <body>
-    <?php
+<?php
     $array=[
         1=>8,
         2=>10,
@@ -21,11 +21,12 @@
         4=>9,
         5=>21,
     ];
-    for ($i=0; $i<=5; $i++)
+    echo '<table border="1"><tr>';
+    for ($i=1; $i<=5; $i++)
     {
-        echo $array[$i]. " ";
+        echo "<th>".$array[$i]."</th>";
     }
-
+    echo "</tr></table>";
     ?>
     
 </body>
